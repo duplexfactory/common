29f7e5a8e57b6ffc213c1faeb5877d2757c30b7c
Victor <victorkwok97@gmail.com>
2023-04-27 19:54:11 +0800

    chore: Rule type

--- input-rules.ts
+++ input-rules.ts
@@ -1,18 +1,21 @@
-import {isNotEmpty, isValidEmail, isValidPhone} from "./helpers";
+import {isEmpty, isNotEmpty, isValidEmail, isValidPhone} from "./helpers";
+
+export type Rule<T> = (v: T) => true | string
+
 
 /**
  * Required
  */
 
-export const requiredRule = (v: any) => !!v || '必須填寫'
-export const requiredInputStringRule = (v: string) => !!v.trim() || '請填寫此欄目'
-export const requiredInputNumberRule = (v: any) => !!v || v === 0 || '請填寫此欄目'
-export const requiredSelectRule = (v: any) => isNotEmpty(v) || '請選擇項目'
-export const requiredCheckboxRule = (v: boolean) => v || '請勾選以上項目'
+export const requiredRule: Rule<any> = (v: any) => !!v || '必須填寫'
+export const requiredInputStringRule: Rule<string> = (v: string) => !!v.trim() || '請填寫此欄目'
+export const requiredInputNumberRule: Rule<any> = (v: any) => !!v || v === 0 || '請填寫此欄目'
+export const requiredSelectRule: Rule<any> = (v: any) => isNotEmpty(v) || '請選擇項目'
+export const requiredCheckboxRule: Rule<boolean> = (v: boolean) => v || '請勾選以上項目'
 
 /**
  * Format
  */
 
-export const formatEmailRule = (v: string) => isValidEmail(v) || '請以正確格式填寫電郵'
-export const formatPhoneRule = (v: string) => isValidPhone(v) || '請以正確格式填寫電話'
+export const formatEmailRule: Rule<string> = (v: string) => isValidEmail(v) || '請以正確格式填寫電郵'
+export const formatPhoneRule: Rule<string> = (v: string) => isValidPhone(v) || '請以正確格式填寫電話'
